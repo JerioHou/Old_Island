3c0706fc0c0996d51b2e33ef2f58e4dea1080957
jerio_hou <houjie20@gmail.com>
2019-06-09 23:38:34 +0800

    first commit

---
+++ models/classic.js
@@ -0,0 +1,14 @@
+import {HTTP} from '../utils/http.js'
+
+class ClassicModel extends HTTP {
+  getLatest(sCallBack) {
+    this.request({
+      url: 'classic/latest',
+      success: (data)=>{
+        sCallBack(data)
+      }
+    })
+  }
+}
+
+export { ClassicModel}
